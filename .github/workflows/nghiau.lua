3b9775912d313d4c322046d127c055294e1416ae
Nghithanh491 <nghithanhau@gmail.com>
2025-11-09 16:21:09 +0700

    Update nghiau.lua

--- .github/workflows/nghiau.lua
+++ .github/workflows/nghiau.lua
@@ -1,8 +1,8 @@
-return {
-   -- Event:
+return {  
+	-- Event:
     CRAFT_EVENT = {"Anti Bee Egg"},
     BUY_TRAVELING_MERCHANT = { "Spooky Chest", "Bee Egg", "Paradise Egg", "Fall Egg", "Loquat", "Feijoa", "Pitcher Plant" },
-    BUY_EVENT_SHOP = { "Safari Egg","Lion", },
+    BUY_EVENT_SHOP = { "Safari Egg", "Lion",},
     DISABLE_REAPER_EVENT = false,
   
     MAX_PLANTS = 150,
@@ -25,19 +25,19 @@ return {
 
     BUY_COSMETIC_SHOP = { ["Cooking Kit"] = 3 },
 
-    BUY_EGGS = { "Safari Egg", "Spooky EgGg", "Jungle Eggg", "Bug Eggg", "Bee Eggg", "Paradise Eggg", "Mythical Eggg", "Rare Summer Eggg", "Common Summer EgGg", "Rare Egg",},
-    PLANT_EGGS = { "Safari Egg", Rare Egg", "Spooky Egg", "Paradise Eggg",},
+    BUY_EGGS = { "Safari Egg", "Spooky EggG", "Jungle EgGg", "Bug EgGg", "Bee EggG", "Paradise EggG", "Rare Summer EggG", "Common Summer Egg", "Rare Egg",},
+    PLANT_EGGS = { "Safari Egg", "Rare Egg", },
     
     BUY_SEED_SHOP = { "Great Pumpkin", "Crimson Thorn", "Romanesco", "Elder Strawberry", "Giant Pinecone", "Burning Bud", "Sugar Apple", "Ember Lily", "Beanstalk", "Cacao", "Pepper", "Mushroom", "Grape", "Mango", "Dragon Fruit", "Cactus", ["Corn"] = 50, ["Coconut"] = 50, ["Bamboo"] = 50, ["Apple"] = 50, ["Pumpkin"] = 50, ["Watermelon"] = 50, ["Daffodil"] = 50, ["Tomato"] = 50, ["Orange Tulip"] = 50, ["Blueberry"] = 50, ["Strawberry"] = 50, ["Carrot"] = 50 },
 
     FAVOURITE_FRUIT_MUTATIONS = {},  -- Stop Autosell
     SKIP_HARVEST_MUTATIONS = {},  -- Stop Harvest
 
-    KEEP_PETS = { "Hydra", ["Rhino"] = 5, "Elephant", "Mummy", "Lich", "Hex Serpent", ["Ghost Bear"] = 8, "Headless Horseman", "Reaper", "Fortune Squirrel", "Chubby Chipmunk", 
+    KEEP_PETS = {"Lion", "Hydra", ["Rhino"] = 5, "Elephant", "Mummy", "Lich", "Hex Serpent", ["Ghost Bear"] = 8, "Headless Horseman", "Reaper", "Fortune Squirrel", "Chubby Chipmunk", 
         "Chinchilla", "Tiger", "Barn Owl", "Swan", "Phoenix", 	
         ["Wisp"] = 5, ["Luminous Sprite"] = 8, "Cockatrice", "Gnome", 
         "Lemon Lion", "Golden Goose", "Kitsune", "Corrupted Kitsune", "French Fry Ferret", 
-        "Lobster Thermidor", "Crocodile", "Hippo", "Lion",
+        "Lobster Thermidor",
 		"T-Rex", "Spinosaurus", "Fennec Fox", "Mimic Octopus", "Disco Bee",
 		"Butterfly", "Raccoon", "Queen Bee", "Dragonfly", "Red Fox",
 		"Ankylosaurus", "Dilophosaurus", "Brontosaurus", "Hyacinth Macaw",
@@ -46,16 +46,16 @@ return {
 		"Raiju", "Chicken Zombie", ["Wasp"] = 5, ["Tarantula Hawk"] = 5,
 		"Axolotl", "Echo Frog", "Hamster", "Maneki-neko", ["Squirrel"] = 2, ["Gorilla Chef"] = 2,
 		"Koi", 
-		["Starfish"] = 10, ["Tanchozuru"] = 2,5,
-		["Rooster"] = 10, ["Seal"] = 3,
+		["Starfish"] = 10, ["Tanchozuru"] = 5,
+		["Rooster"] = 8, ["Seal"] = 3,
 		["Peacock"] = 5, ["Ostrich"] = 5, ["Scarlet Macaw"] = 3, ["Sunny-Side Chicken"] = 2, "Capybara",["Golden Lab"] = 8},
     KEEP_PETS_WEIGHT = 7,
     KEEP_PETS_AGE = 75,
-   EQUIP_PETS = { {"Rooster", 8, 1}, {"Seal", 8, 2}, {"Starfish", 8, 3}},
-    USE_PETS_FOR_UPGRADE_SLOT = { "Capybara", "Starfish", "Rooster", "Seal", "Squirrel"},
-    REMOVE_PET_MAX_UPGRADE = { "Capybara", "Starfish", "Seal"},  -- Unequip from garden
+    EQUIP_PETS = {{"Starfish", 8, 1},{"Rooster", 8, 2} },
+    USE_PETS_FOR_UPGRADE_SLOT = { "Capybara", "Starfish",},
+    REMOVE_PET_MAX_UPGRADE = { "Capybara", "Starfish", "Golden Lab", },  -- Unequip from garden
 
-    BUY_GEAR_SHOP = { "Grandmaster Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Basic Sprinkler", ["Harvest Tool"] = 10, ["Trading Ticket"] = 100},
+    BUY_GEAR_SHOP = { "Grandmaster Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Basic Sprinkler", ["Harvest Tool"] = 10, ["Trading Ticket"] = 20},
     USE_SPRINKLER = { "Basic Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Grandmaster Sprinkler" },
 
     PET_WEBHOOK_URL = "",
