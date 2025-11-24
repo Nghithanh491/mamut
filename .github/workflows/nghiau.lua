00e4e51a7627782039d8d92286ab78120cc1692d
Nghithanh491 <nghithanhau@gmail.com>
2025-11-24 12:59:36 +0700

    Update nghiau.lua

--- .github/workflows/nghiau.lua
+++ .github/workflows/nghiau.lua
@@ -1,10 +1,12 @@
 return {  
--- Event:
-   BUY_TRAVELING_MERCHANT = { "Bee Egg", "Paradise Egg", "Fall Egg", "Loquat", "Feijoa", "Pitcher Plant" },
-    PLANT_EVENT_TREES = true,
+ -- Event:
+    CRAFT_EVENT = {"Anti Bee Egg",},
+    BUY_TRAVELING_MERCHANT = { "Bee Egg", "Paradise Egg", "Fall Egg", },
+	PLANT_EVENT_TREES = true,
   
-    MAX_PLANTS = 150,
-    DESTROY_UNTIL_MIN_PLANTS = 130,
+  	GET_HONEY = true,
+    MAX_PLANTS = 120,
+    DESTROY_UNTIL_MIN_PLANTS = 90,
     DELETE_PLANTS_AFTER_MAX = {},
     KEEP_SEEDS = { "Bone Blossom" },
     KEEP_SEEDS_AFTER_MAX_PLANTS = {"Strawberry"},
@@ -20,12 +22,12 @@ return {
     MAX_REBIRTH_SHECKLES = 0,
     ADD_FRIEND = false,
     OPEN_ALL_SEED_PACK = true,
-    COLLECT_FRUIT_PER_SECOND = 3,
+    COLLECT_FRUIT_PER_SECOND = 5,
 
     BUY_COSMETIC_SHOP = { ["Cooking Kit"] = 3 },
 
-    BUY_EGGS = { "Gem Egg", "Jungle Egg", "Fall Eggg", "Bee Egg", "Common Summer Eggg", "Rare Summer Eggg", "Rare Eggg", ["Common Eggg"] = 10, "Uncommon Eggg", "Paradise Egg", "Legendary Eggg", "Mythical Eggg"},
-    PLANT_EGGS = { "Paradise Egg", "Gem Egg", "Safari Egg", "Anti Bee Egg","Bee Egg" "Spooky Egg", "Jungle Egg", "Fall Egg", "Bug Egg", },
+    BUY_EGGS = { "Gem Egg", "Jungle Egg", "Fall Egg", "Bee Egg", "Common Summer Egg", "Rare Summer Egg", "Rare Egg", ["Common Egg"] = 10, ["Uncommon Egg"] = 10, "Paradise Egg", "Legendary Egg", "Mythical Egg", "Bug Egg"},
+    PLANT_EGGS = {"Anti Bee Egg", "Spooky Egg", "Safari Egg", "Paradise Egg", "Gem Egg","Jungle Egg", "Bug Egg"},
     
     BUY_SEED_SHOP = { ["Great Pumpkin"] = 2, ["Crimson Thorn"] = 2, ["Romanesco"] = 2, ["Elder Strawberry"] = 2, ["Giant Pinecone"] = 2, ["Burning Bud"] = 2, ["Sugar Apple"] = 5, ["Ember Lily"] =5, ["Beanstalk"] = 5, ["Cacao"] = 10, ["Pepper"] = 10, ["Mushroom"] = 10, ["Grape"] = 5, ["Mango"] = 10, ["Dragon Fruit"] = 5, ["Cactus"] = 5, ["Corn"] = 30, ["Coconut"] = 30, ["Bamboo"] = 30, ["Apple"] = 30, ["Pumpkin"] = 30, ["Watermelon"] = 30, ["Daffodil"] = 30, ["Tomato"] = 30, ["Orange Tulip"] = 30, ["Blueberry"] = 50, ["Strawberry"] = 30, ["Carrot"] = 30 },
   
@@ -34,17 +36,18 @@ return {
     FAVOURITE_FRUIT_MUTATIONS = {},  -- Stop Autosell
     SKIP_HARVEST_MUTATIONS = {},  -- Stop Harvest
 
-    KEEP_PETS = { ["Magpie"] = 5, ["Pack Mule"] = 5,"Chimera", "Bearded Dragon", "Goblin Miner", "Ruby Squid", "Diamond Panther", "Lion", "Hydra", "Elephant", "Mummy", "Lich", "Hex Serpent", ["Ghost Bear"] = 8, "Headless Horseman", "Reaper", "Fortune Squirrel", 
+    KEEP_PETS = { ["Magpie"] = 5, ["Pack Mule"] = 5, "Chimera", "Bearded Dragon", "Goblin Miner", "Ruby Squid", "Diamond Panther", "Lion", "Hydra", "Elephant", "Mummy", "Lich", "Hex Serpent", ["Ghost Bear"] = 8, "Headless Horseman", "Reaper", "Fortune Squirrel", 
         "Chinchilla", "Tiger", "Barn Owl", "Swan", "Phoenix", 	
         ["Wisp"] = 5, ["Luminous Sprite"] = 8, "Cockatrice", "Gnome", 
         "Lemon Lion", "Golden Goose", "Kitsune", "Corrupted Kitsune", "French Fry Ferret", 
         "Lobster Thermidor",
 		"T-Rex", "Spinosaurus", "Fennec Fox", "Mimic Octopus", "Disco Bee",
-		"Butterfly", "Raccoon", "Queen Bee", "Mega Queen Bee", "Dragonfly", "Red Fox",
+		"Butterfly", "Raccoon", "Mega Queen Bee", "Dragonfly", "Red Fox",
 		"Ankylosaurus", "Dilophosaurus", "Brontosaurus", "Hyacinth Macaw",
 		"Moon Cat",
+    	"Bee", "Honey Bee", "Petal Bee", "Queen Bee",
 		"Night Owl", "Blood Owl", "Blood Kiwi", "Cooked Owl",
-		"Raiju", "Chicken Zombie", ["Wasp"] = 5, ["Tarantula Hawk"] = 5,
+		"Raiju", "Chicken Zombie", "Wasp", "Tarantula Hawk",
 		"Axolotl", "Echo Frog", "Hamster", "Maneki-neko",
 		"Koi", "Scarlet Macaw",
 		["Starfish"] = 12, 
@@ -52,7 +55,7 @@ return {
 		"Peacock", "Ostrich", ["Sunny-Side Chicken"] = 2, "Capybara"},
     KEEP_PETS_WEIGHT = 7,
     KEEP_PETS_AGE = 75,
-    EQUIP_PETS = {{"Starfish", 8, 1}, {"Rooster", 8, 2}, },
+    EQUIP_PETS = {{"Queen Bee", 8, 1},{"Honey Bee", 8, 2},{"Petal Bee", 8, 3},{"Bee", 8, 4}, },
     LEVELUP_LOLLIPOP_MIN_LEVEL = 50,
     USE_PETS_FOR_UPGRADE_SLOT = { "Starfish", "Rooster"},
     REMOVE_PET_MAX_UPGRADE = { "Starfish",},  -- Unequip from garden
